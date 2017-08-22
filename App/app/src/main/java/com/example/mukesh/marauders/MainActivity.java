3709f437785a8e719ac404ca5a5cf06efb6f1717
Mukesh Kharita <mukeshnithcse@gmail.com>
2017-08-22 14:32:55 +0530

    Add map

--- App/app/src/main/java/com/example/mukesh/marauders/MainActivity.java
+++ App/app/src/main/java/com/example/mukesh/marauders/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.mukesh.marauders;
 
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
